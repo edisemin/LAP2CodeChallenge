261f5a754ab1dec25d30018cda1670aac0de35c2
“crlnvls” <cveloso@hotmail.co.uk>
2022-08-10 11:51:53 +0100

    Add new changes

---
+++ api/controllers/post.js
@@ -0,0 +1,52 @@
+const Post = require("../models/post");
+
+async function index(req, res) {
+  try {
+    const data = await Post.all;
+    res.json({
+      success: true,
+      posts: data,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err,
+    });
+  }
+}
+
+async function create(req, res) {
+  try {
+    const data = await Post.create(req.body);
+    res.json({
+      success: true,
+      post: data,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err,
+    });
+  }
+}
+
+async function show(req, res) {
+  try {
+    const data = await Post.getOneById(req.params.id);
+    res.json({
+      success: true,
+      post: data,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err,
+    });
+  }
+}
+
+module.exports = {
+  index,
+  create,
+  show,
+};
